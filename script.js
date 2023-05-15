77c1d0c6874627d95958ca7cab5388a1170b0213
jaltadeepak <deepaksingh131102@gmail.com>
2023-05-15 20:01:01 +0530

    added line and game over sound

--- script.js
+++ script.js
@@ -11,20 +11,23 @@ const changeTurn = ()=>{
 const checkWin = ()=>{
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 1, 2, 7, 6.5, 0],
+        [3, 4, 5, 7, 20.5, 0],
+        [6, 7, 8, 7, 34.5, 0],
+        [0, 3, 6, -7, 20.5, 90],
+        [1, 4, 7, 7, 20.5, 90],
+        [2, 5, 8, 21, 20.5, 90],
+        [0, 4, 8, 7, 20.5, 45],
+        [2, 4, 6, 7, 20.5, 135],
     ]
     wins.forEach(e =>{
         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[2]].innerText !== "")){
             document.querySelector('.info').innerText = boxtext[e[2]].innerText + " wins"
+            gameover.play();
             isGameOver = true
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+            document.querySelector(".line").style.width="28vw"
+            document.querySelector(".line").style.transform=`translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
         }
     })
 }
@@ -52,6 +55,7 @@ reset.addEventListener('click', ()=>{
     });
     turn = "X";
     isGameOver = false;
+    document.querySelector(".line").style.width="0vw"
     document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
 })
